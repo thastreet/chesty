1f184abf3fbe27b4a417edacbbfc45de5b4ebefe
Mathieu Larue <mlarue@mirego.com>
2022-03-30 22:16:17 -0400

    Test buffer

--- bot.js
+++ bot.js
@@ -161,7 +161,7 @@ function play(message, song) {
 
 	const stream = ytdl(song.url, {
 		quality: 'highestaudio',
-		highWaterMark: 1 << 25
+		highWaterMark: 1 << 10
 	})
 	playingSong.player.play(createAudioResource(stream, { inputType: StreamType.Arbitrary }));
 	playingSong.player.on(AudioPlayerStatus.Idle, () => playNextSong(message))
