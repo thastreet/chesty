a5f77b8a5a89c5194c7fe7518d4925ff3a7c9230
Mathieu Larue <mat_1794@hotmail.com>
2023-03-19 14:57:52 -0400

    Queue

--- player.js
+++ player.js
@@ -3,7 +3,7 @@ const ytdl = require("ytdl-core");
 const { guild_id } = require("./config.json");
 
 const { getMetadata } = require("./spotify.js");
-const { getVideoId } = require("./youtube.js");
+const { getVideoId, getPlaylistIds } = require("./youtube.js");
 
 const CommandNames = {
     Play: "play",
@@ -34,14 +34,23 @@ function resolveQuery(queue, interaction, query, player) {
     if (query.includes("open.spotify.com")) {
         getMetadata(query, (trackName, artist) => {
             getVideoId(`${artist} ${trackName} audio`, (videoId) => {
-                playYoutubeVideoId(videoId, queue, interaction, player);
+                const youtubeUrl = getYoutubeUrl(videoId)
+                playYoutubeUrls([youtubeUrl], queue, interaction, player);
             });
         });
+    } else if (query.includes("youtube.com") && query.includes("list=")) {
+        getPlaylistIds(query, (ids) => {
+            const urls = ids.map((id) => {
+                return getYoutubeUrl(id);
+            });
+            playYoutubeUrls(urls, queue, interaction, player);
+        });
     } else if (query.includes("youtube.com")) {
-        playYoutubeUrl(queue, query, interaction, player);
+        playYoutubeUrls([query], queue, interaction, player);
     } else {
         getVideoId(`${query} audio`, (videoId) => {
-            playYoutubeVideoId(videoId, queue, interaction, player);
+            const youtubeUrl = getYoutubeUrl(videoId)
+            playYoutubeUrls([youtubeUrl], queue, interaction, player);
         });
     }
 }
@@ -61,26 +70,34 @@ function clear(queue, interaction) {
     sendMessage("The queue has been cleared!", interaction);
 }
 
-function playYoutubeVideoId(videoId, queue, interaction, player) {
-    playYoutubeUrl(queue, `https://www.youtube.com/watch?v=${videoId}`, interaction, player);
+function getYoutubeUrl(videoId) {
+    return `https://www.youtube.com/watch?v=${videoId}`
 }
 
-function playYoutubeUrl(queue, url, interaction, player) {
-    ytdl.getInfo(url).then((songInfo) => {
-        const song = {
-            title: songInfo.videoDetails.title,
-            url: songInfo.videoDetails.video_url,
-        };
+function playYoutubeUrls(urls, queue, interaction, player) {
+    const firstSong = urls.slice(0, 1)[0];
+    const otherSongs = urls.slice(1, urls.length);
 
-        if (player.state.status == AudioPlayerStatus.Idle) {
-            joinVoiceChannelAndPlaySong(song, queue, interaction, player);
-        } else {
-            addSongToQueue(song, queue, interaction);
-        }
-    });
+    const newQueue = [];
+
+    if (player.state.status != AudioPlayerStatus.Idle) {
+        newQueue.push(firstSong);
+    }
+
+    newQueue.push(...otherSongs);
+
+    queue.push(...newQueue);
+
+    if (player.state.status == AudioPlayerStatus.Idle) {
+        joinVoiceChannelAndPlaySong(firstSong, queue, interaction, player, newQueue);
+    } else if (newQueue.length == 1) {
+        sendMessage(`Added ${newQueue[0]} to queue`, interaction);
+    } else {
+        sendMessage(`Added ${newQueue.length} songs to queue`, interaction);
+    }
 }
 
-function joinVoiceChannelAndPlaySong(song, queue, interaction, player) {
+function joinVoiceChannelAndPlaySong(url, queue, interaction, player, newQueue) {
     try {
         const voiceChannel = interaction.member.voice.channel;
 
@@ -92,40 +109,35 @@ function joinVoiceChannelAndPlaySong(song, queue, interaction, player) {
 
         const playNextSong = () => {
             if (queue.length > 0) {
-                playSong(queue.shift(), null, connection, player, playNextSong);
+                playSong(queue.shift(), null, connection, player, playNextSong, []);
             } else {
                 console.log("No more songs to play");
             }
         };
 
-        playSong(song, interaction, connection, player, playNextSong);
+        playSong(url, interaction, connection, player, playNextSong, newQueue);
     } catch (err) {
         console.error(err);
     }
 }
 
-function addSongToQueue(song, queue, interaction) {
-    queue.push(song);
-    sendMessage(`Added to queue: ${song.title}`, interaction);
-}
+function playSong(url, interaction, connection, player, playNextSong, newQueue) {
+    sendMessage(`Playing: ${url}` + (newQueue.length > 0 ? `, added ${newQueue.length} songs to queue` : ""), interaction);
 
-function playSong(song, interaction, connection, player, playNextSong) {
-    console.log(`Playing: ${song.title}`);
-    if (interaction) {
-        interaction.reply(`Playing: ${song.url}`);
-    }
-
-    const stream = ytdl(song.url, {
+    const stream = ytdl(url, {
         quality: "highestaudio",
         highWaterMark: 1 << 25
     });
 
     player.play(createAudioResource(stream, { inputType: StreamType.Arbitrary }));
+    player.removeAllListeners();
     player.on(AudioPlayerStatus.Idle, playNextSong);
     connection.subscribe(player);
 }
 
 function sendMessage(message, interaction) {
     console.log(message);
-    interaction.reply(message);
+    if (interaction) {
+        interaction.reply(message);
+    }
 }
--- youtube.js
+++ youtube.js
@@ -1,5 +1,6 @@
 const { youtube_api_key } = require("./config.json");
 const axios = require("axios");
+var url = require('url');
 
 module.exports = {
     getVideoId: function (query, onIdReceived) {
@@ -7,6 +8,20 @@ module.exports = {
             .get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${encodeURIComponent(query)}&key=${youtube_api_key}`)
             .then(res => {
                 onIdReceived(res.data.items[0].id.videoId);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    },
+    getPlaylistIds: function (query, onIdsReceived) {
+        const listId = url.parse(query, true).query.list
+
+        axios
+            .get(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=${listId}&key=${youtube_api_key}`)
+            .then(res => {
+                onIdsReceived(res.data.items.map((item) => {
+                    return item.snippet.resourceId.videoId;
+                }));
             })
             .catch(error => {
                 console.error(error);
