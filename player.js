3b4277f834c8134eec747d1340f0a09438ad8adb
Mathieu Larue <mat_1794@hotmail.com>
2023-03-19 11:55:49 -0400

    Cleanup

--- player.js
+++ player.js
@@ -6,19 +6,17 @@ const { getMetadata } = require("./spotify.js");
 const { getVideoId } = require("./youtube.js");
 
 const CommandNames = {
-	Play: "play",
-	Skip: "skip",
-	Clear: "clear"
+    Play: "play",
+    Skip: "skip",
+    Clear: "clear"
 };
 
 function listenForInteraction(client, queue, player) {
     client.once("interactionCreate", async interaction => {
         if (interaction.commandName === CommandNames.Play) {
-            const query = interaction.options.getString("query");
-            resolveQuery(queue, interaction, query, player);
+            resolveQuery(queue, interaction, interaction.options.getString("query"), player);
         } else if (interaction.commandName === CommandNames.Skip) {
-            interaction.reply("Skipping!");
-            player.stop();
+            skip(player, interaction);
         } else if (interaction.commandName === CommandNames.Clear) {
             clear(queue, interaction);
         }
@@ -33,79 +31,101 @@ module.exports = {
 };
 
 function resolveQuery(queue, interaction, query, player) {
-	if (query.includes("open.spotify.com")) {
-		getMetadata(query, (trackName, artist) => {
-			getVideoId(`${artist} ${trackName} audio`, (videoId) => {
-				playYoutubeVideoId(videoId, queue, interaction, player);
-			});
-		});
-	} else if (query.includes("youtube.com")) {
-		playYoutubeUrl(queue, query, interaction, player);
-	} else {
-		getVideoId(`${query} audio`, (videoId) => {
-			playYoutubeVideoId(videoId, queue, interaction, player);
-		});
-	}
+    if (query.includes("open.spotify.com")) {
+        getMetadata(query, (trackName, artist) => {
+            getVideoId(`${artist} ${trackName} audio`, (videoId) => {
+                playYoutubeVideoId(videoId, queue, interaction, player);
+            });
+        });
+    } else if (query.includes("youtube.com")) {
+        playYoutubeUrl(queue, query, interaction, player);
+    } else {
+        getVideoId(`${query} audio`, (videoId) => {
+            playYoutubeVideoId(videoId, queue, interaction, player);
+        });
+    }
 }
 
-function playYoutubeVideoId(videoId, queue, interaction, player) {
-	playYoutubeUrl(queue, `https://www.youtube.com/watch?v=${videoId}`, interaction, player);
-}
-
-function playYoutubeUrl(queue, url, interaction, player) {
-	ytdl.getInfo(url).then((songInfo) => {
-		const song = {
-			title: songInfo.videoDetails.title,
-			url: songInfo.videoDetails.video_url,
-		};
-
-		if (player.state.status == AudioPlayerStatus.Idle) {
-			try {
-				const voiceChannel = interaction.member.voice.channel;
-
-				const connection = joinVoiceChannel({
-					channelId: voiceChannel.id,
-					guildId: guild_id,
-					adapterCreator: voiceChannel.guild.voiceAdapterCreator
-				});
-
-				const playNextSong = () => {
-					if (queue.length > 0) {
-						play(null, queue.shift(), connection, player, playNextSong);
-						console.log(queue);
-					}
-				};
-
-				play(interaction, song, connection, player, playNextSong);
-			} catch (err) {
-				console.log(err);
-				interaction.reply(err);
-			}
-		} else {
-			queue.push(song);
-			console.log(queue);
-			interaction.reply(`${song.title} has been added to the queue!`);
-		}
-	});
+function skip(player, interaction) {
+    sendMessage("Skipping!", interaction);
+    player.stop();
 }
 
 function clear(queue, interaction) {
-	if (queue.length == 0) return;
-	queue = [];
-	console.log(queue);
-	interaction.reply("The queue has been cleared!");
+    if (queue.length == 0) return;
+
+    while (queue.length > 0) {
+        queue.pop();
+    }
+
+    sendMessage("The queue has been cleared!", interaction);
 }
 
-function play(interaction, song, connection, player, playNextSong) {
-	if (interaction) {
-		interaction.reply(`Yezzir playing: ${song.url}`);
-	}
+function playYoutubeVideoId(videoId, queue, interaction, player) {
+    playYoutubeUrl(queue, `https://www.youtube.com/watch?v=${videoId}`, interaction, player);
+}
 
-	const stream = ytdl(song.url, {
-		quality: "highestaudio",
-		highWaterMark: 1 << 25
-	});
-	player.play(createAudioResource(stream, { inputType: StreamType.Arbitrary }));
-	player.on(AudioPlayerStatus.Idle, playNextSong);
-	connection.subscribe(player);
+function playYoutubeUrl(queue, url, interaction, player) {
+    ytdl.getInfo(url).then((songInfo) => {
+        const song = {
+            title: songInfo.videoDetails.title,
+            url: songInfo.videoDetails.video_url,
+        };
+
+        if (player.state.status == AudioPlayerStatus.Idle) {
+            joinVoiceChannelAndPlaySong(song, queue, interaction, player);
+        } else {
+            addSongToQueue(song, queue, interaction);
+        }
+    });
+}
+
+function joinVoiceChannelAndPlaySong(song, queue, interaction, player) {
+    try {
+        const voiceChannel = interaction.member.voice.channel;
+
+        const connection = joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: guild_id,
+            adapterCreator: voiceChannel.guild.voiceAdapterCreator
+        });
+
+        const playNextSong = () => {
+            if (queue.length > 0) {
+                playSong(queue.shift(), null, connection, player, playNextSong);
+            } else {
+                console.log("No more songs to play");
+            }
+        };
+
+        playSong(song, interaction, connection, player, playNextSong);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function addSongToQueue(song, queue, interaction) {
+    queue.push(song);
+    sendMessage(`Added to queue: ${song.title}`, interaction);
+}
+
+function playSong(song, interaction, connection, player, playNextSong) {
+    console.log(`Playing: ${song.title}`);
+    if (interaction) {
+        interaction.reply(`Playing: ${song.url}`);
+    }
+
+    const stream = ytdl(song.url, {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25
+    });
+
+    player.play(createAudioResource(stream, { inputType: StreamType.Arbitrary }));
+    player.on(AudioPlayerStatus.Idle, playNextSong);
+    connection.subscribe(player);
+}
+
+function sendMessage(message, interaction) {
+    console.log(message);
+    interaction.reply(message);
 }
